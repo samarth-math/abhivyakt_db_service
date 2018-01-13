c4aa37a0fc2fcf87d566a0635b774aef6ae42c99
Rishabh <rishabhbaid1@gmail.com>
2018-01-13 16:31:53 +0530

    Adding more kahanis by Premchand, also cleaned up code to use helperfunction module.
    Total Number of kahanis now in db: 243

--- crawling/kahani.py
+++ crawling/kahani.py
@@ -3,45 +3,14 @@ import requests
 from bs4 import BeautifulSoup, Tag, NavigableString
 from pymongo import MongoClient
 from pprint import pprint
+import helperfunctions as H
 
-baseURL = "http://www.hindikibindi.com/content/manoranjan/stories/"
-numKavitaG = 0
-kavitalist = []
-invalidKavitaList = []
-invalidKavitaG = 0
-invalidBirthSpanG = 0
-invalidBirthSpanList = []
-
-host = 'localhost'
-PORT = 27017
-client = MongoClient(host, PORT)
-db = client.literature
-serverStatusResult = db.command("serverStatus")
-pprint(serverStatusResult)
-collection = db['kahani']
-cursor = collection.find({})
-c = 0
-for document in cursor:
-    c = c + 1
-    print(document)
-
-print("Number of documents: ", c)
-
-
-def getSoup(url):
-    try:
-        page = requests.get(url)
-        encoding = page.encoding if 'charset' in page.headers.get(
-            'content-type', '').lower() else None
-    except requests.exceptions.ConnectionError as r:
-        r.status_code = "Connection Refused"
-        return None
-    soup = BeautifulSoup(page.content, 'lxml', from_encoding=encoding)
-    return soup
+collection = H.initializeDB('literature', 'kahani')
 
 
 def parseKahani():
-    soup = getSoup(baseURL)
+    baseURL = "http://www.hindikibindi.com/content/manoranjan/stories/"
+    soup = H.getSoup(baseURL)
     div = soup.find('div', {'class': 'col-lg-12'})
     p = div.find_next('p').find_all('a')
     for a in p:
@@ -49,7 +18,7 @@ def parseKahani():
         kahaniText = u''
         title = u''
         absURL = baseURL + a.get('href')
-        soup = getSoup(absURL)
+        soup = H.getSoup(absURL)
         div = soup.find('div', {'class': 'col-lg-12'})
         p = div.find_next('p').getText()
         title = p.partition('\n')[0]
@@ -68,16 +37,37 @@ def parseKahani():
             'title': title,
             'kahaniText': kahaniText
         }
-        saveToMongoDB(entry)
+        H.saveToMongoDB(collection, entry)
 
 
-def saveToMongoDB(entry):
-    global db
-    db.kahani.insert_one(entry)
+def parsePremchandKahani():
+    global collection
+    url = 'http://premchand.kahaani.org/2009/09/contents.html'
+    soup = H.getSoup(url)
+    div = soup.find('div', {'class': 'nobrtable'})
+    li = div.find_next('tr').find_all('li')
+    count = 0
+    for links in li:
+        print(links.a.get('href'))
+        soup = H.getSoup(links.a.get('href'))
+        div = soup.find('div', {'class': 'post-body entry-content'})
+        kahaniText = div.getText()
+        title = soup.find('meta', property='og:title')["content"]
+        print(title)
+        print(kahaniText)
+        entry = {
+            "title": title,
+            "kahaniText": kahaniText,
+            "author":  "Munshi Premchand"
+        }
+        H.saveToMongoDB(collection, entry)
+        count += 1
+    print("No. of links: ", count)
 
 
 try:
     # parseKahani()
-    doNothing = 1
+    # parsePremchandKahani()
+    H.viewCollection(collection)
 finally:
     print("Completed")
