1c1bcf36ac805175d18dcf42d4eaec79587ac3a9
shubham <shubhamgupta5893@gmail.com>
2018-03-28 00:32:50 +0530

    added view for dictionary

--- api/routes/dictionary.py
+++ api/routes/dictionary.py
@@ -3,7 +3,8 @@ from flask import Flask, url_for,render_template
 from . import routes
 from flask import request
 from flask import jsonify
-
+import json
+import pdb
 @routes.route('/dictionary', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
 def api_dictionary():
     if request.method == 'GET':
@@ -16,10 +17,14 @@ def api_dictionary():
         nextItem = None
         content = word
         data, hasMore, lastItem = Dictionary.getWord(content, limit, nextItem)
-        print('data: ', data)
-        return jsonify(
-            data = data,
-            hasMore = hasMore
-        )
+	data = json.loads(data)
+	data_render = {}
+	data_render["key"] = data[0]["key"]
+	data_render["meanings"] = data[0]["meanings"]
+	return render_template('dictionary_2.html',poem=data_render)
+#        return jsonify(
+#            data = data,
+#            hasMore = hasMore
+#        )
     else:
         return 'This HTTP verb is currently not supported.'
