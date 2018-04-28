3b5b7347512bce0f6a18e4dfb08e74cd849d1163
Rishabh Baid <rishabhbaid1@gmail.com>
2018-04-28 03:38:33 +0000

    Adding error handler for Status Code: 404.     TODO: Add error handlers for all status code and make custom     error pages to be presented to users.

--- api/app.py
+++ api/app.py
@@ -26,5 +26,9 @@ def api_article(kavitaID):
     return 'You are reading ' + kavitaID
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(threaded=True)
