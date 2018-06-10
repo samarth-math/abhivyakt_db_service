71768a2402549b11db70a30ced62ff2997be4592
Mathur <samarth.math@gmail.com>
2018-06-10 14:47:36 -0700

    separate out template

--- api/js/vue/kahani.js
+++ api/js/vue/kahani.js
@@ -45,54 +45,17 @@ var baseComponent = Vue.extend({
     prev: function() {
       this.current = this.current - 1 >= 0 ? this.current - 1 : 0;
     }
-  },
-  delimiters: ["[{", "}]"]
+  }
 });
 
-var kahaniComponent = baseComponent.extend({
-  template: `
-<div>
-<ul v-if="errors && errors.length">
-  <li v-for="error of errors">
-    [{error.message}]
-  </li>
-</ul>
-
-<div class="error-message" v-if="errors && errors.length">
-  <div class="alert alert-danger alert-dismissible">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <span v-for="error of errors">
-      [{error.message}]
-    </span>
-  </div>
-</div>
-
-<div v-if="loaded && loaded.length" class="row justify-content-center">
-  <div class="col-lg-8">
-    <h3>
-      <i class="fas fa-circle space-bottom-md color-pink"></i>
-      <span>[{loaded[current].title}]</span>
-    </h3>
-    <h3 class="sub-heading2">[{loaded[current].author}]</h3>
-    <center>
-      <span v-on:click="prev">
-        <i class="fas fa-chevron-circle-left color-change-hover color-pink"></i>
-      </span>
-      <span v-on:click="next">
-        <i class="fas fa-chevron-circle-right color-change-hover color-pink"></i>
-      </span>
-    </center>
-    [{loaded[current].kahaniText}]
-  </div>
-</div>
-</div>`
-});
+var kahaniComponent = baseComponent.extend({});
 
 new Vue({
   el: "#kahani",
   components: {
     "kahani-component": kahaniComponent
-  }
+  },
+  delimiters: ["[{", "}]"]
 });
 
 /*
