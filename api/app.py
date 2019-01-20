b0cafd8a3f040c5c0ac1d1d989059bcc20d6bb3e
Rishabh Baid <rishabhbaid1@gmail.com>
2019-01-20 18:24:27 +0000

    Merged in feature/sentry-integration (pull request #71)
    Feature/sentry integration
    
    * Changes for sentry integration.
    
        The data source name is currently assigned to my personal account,
        this will be changed to abhivyakt account once we set it up.
    
    * Changed dsn url. Now, Sentry is linked with Abhivyakt gmail account.
    
    * Adding comments.
    
    Approved-by: Samarth Mathur <samarth.math@gmail.com>

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import sentry_sdk
 from .routes import routes
 from flask import Flask, render_template, send_from_directory
+from sentry_sdk.integrations.flask import FlaskIntegration
 # This file is where the program starts
 # but each API has it's own file in the routes
 # sub directory.
@@ -10,6 +12,15 @@ from flask import Flask, render_template, send_from_directory
 # We will only keep the route API here, and eventually
 # remove all others
 
+# Refer url: https://docs.sentry.io/error-reporting/quickstart/?platform=python#configure-the-sdk
+# for configuration and additional parameter supported. Here we are
+# initializing the Sentry sdk with Data Source Name linked to Abhivyakt
+# account. If we migrate to a different id or account, only this value
+# needs to be changed.
+sentry_sdk.init(
+    dsn="https://2f37e9c5248c451392bd9956dad14444@sentry.io/1375493",
+    integrations=[FlaskIntegration()]
+)
 app = Flask(__name__)
 app.register_blueprint(routes)
 
